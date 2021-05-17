832f026bb060beaa5a43e4ef2a6697a138cadc02
pashaenko <pashaenko@icloud.com>
2021-05-17 11:27:46 +0300

    add answers

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,13 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    UInt8 answers = 0;
+    for (NSInteger q = 0; q < 8; q++) {
+        if (n % 2) {
+            answers+=pow(2,7 - q);
+        }
+        n /=2;
+    }
+    return answers;
 }
+
--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,14 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
-}
-
+    NSInteger result = 0;
+       for (NSInteger s = 0; s < array.count; s++) {
+           for (NSInteger k = s + 1; k < array.count; k++) {
+               if (ABS(array[s].intValue - array[k].intValue) == number.intValue ) {
+                   result++;
+               }
+           }
+       }
+       return result;
+   }
 @end
